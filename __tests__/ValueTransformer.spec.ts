16de9a402a29844e90bd7643f17f4b93eb95bd32
Mihail Cristian Dumitru <doomyxx@gmail.com>
2018-08-01 23:35:32 +0300

    Added value mapping transformer tests

--- __tests__/ValueTransformer.spec.ts
+++ __tests__/ValueTransformer.spec.ts
@@ -42,3 +42,52 @@ describe("ValueTransformer", () => {
         expect(transformer.reverseTransformedValue("foobar")).toEqual("foo");
     });
 });
+
+describe("value mapping transformer", () => {
+    enum AdditionTypes {
+        Negative = -1,
+        Zero = 0,
+        Positive = 1,
+        Default = 42,
+    }
+
+    const values = {
+        "negative": AdditionTypes.Negative,
+        "zero": AdditionTypes.Zero,
+        "positive": AdditionTypes.Positive,
+    };
+
+    let transformer: ValueTransformer;
+
+    beforeEach(() => {
+        transformer = ValueTransformer.valueMappingTransformer(values);
+    });
+
+    it("should transform strings into enum values", () => {
+        expect(transformer.transformedValue("negative")).toEqual(AdditionTypes.Negative);
+        expect(transformer.transformedValue("zero")).toEqual(AdditionTypes.Zero);
+        expect(transformer.transformedValue("positive")).toEqual(AdditionTypes.Positive);
+    });
+
+    it("should transform enum values into strings", () => {
+        expect(transformer.allowsReverseTransformation()).toBeTruthy();
+
+        expect(transformer.reverseTransformedValue(AdditionTypes.Negative)).toEqual("negative");
+        expect(transformer.reverseTransformedValue(AdditionTypes.Zero)).toEqual("zero");
+        expect(transformer.reverseTransformedValue(AdditionTypes.Positive)).toEqual("positive");
+    });
+
+    describe("default values", () => {
+        beforeEach(() => {
+            transformer = ValueTransformer.valueMappingTransformer(values, AdditionTypes.Default, "default")
+        });
+
+        it("should transform unknown strings into the default enum value", () => {
+            expect(transformer.transformedValue("unknown")).toEqual(AdditionTypes.Default);
+        });
+
+        it("should transform the default enum value into the default string", () => {
+            expect(transformer.reverseTransformedValue(AdditionTypes.Default)).toEqual("default");
+        });
+    });
+});
